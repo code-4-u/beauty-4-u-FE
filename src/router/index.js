eb859d630eb10ff758ef6e4d4f54ff62b24c0f59
seolbin01 <se01bin@suwon.ac.kr>
2024-12-05 19:30:13 +0900

    feat: 라우터 추가 #7

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/views/Home.vue";
 
+import userRoutes from './user.js';
+
 const routes = [
     {
         path : '/',
         component : Home
-    }
+    },
+    ...userRoutes
 ];
 
 const router = createRouter({
