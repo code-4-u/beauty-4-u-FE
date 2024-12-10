a1a2833c1b4ef71a22f999926e4256cd764fd6ae
leebackcoding <lcy2825@gmail.com>
2024-12-11 00:03:48 +0900

    chore: 부트스트랩 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import { createApp } from 'vue';
-import { createPinia } from "pinia";
+import {createApp} from 'vue';
+import {createPinia} from "pinia";
 import App from './App.vue';
 import router from "@/router/index.js";
-import { setupFontAwesome } from './plugins/fontawesome'
+import {setupFontAwesome} from './plugins/fontawesome'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App);
 const pinia= createPinia();
 
 app.use(router);
 app.use(pinia);
+app.use(BootstrapVue3);
 
 setupFontAwesome(app);
 
