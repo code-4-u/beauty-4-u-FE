d48ca3b30cf000cd24121a5d29ede8c51ac891bc
leebackcoding <lcy2825@gmail.com>
2024-12-16 15:07:36 +0900

    feat: 공지사항 라우터 추가

--- src/router/board.js
+++ src/router/board.js
@@ -3,6 +3,8 @@ import boardHome from "@/views/board/BoardHome.vue";
 import QAndAList from "@/views/board/QAndAList.vue";
 import FAQList from "@/views/board/FAQList.vue";
 import informDetail from "@/components/board/inform/InformDetail.vue";
+import InformSave from "@/components/board/inform/InformSave.vue";
+import InformUpdate from "@/components/board/inform/informUpdate.vue";
 
 export default [
     {
@@ -14,8 +16,16 @@ export default [
                 component: InformList
             },
             {
+                path: 'inform/save',
+                component: InformSave
+            },
+            {
                 path: 'inform/:informId',
                 component: informDetail
+            },
+            {
+                path: 'inform/:informId/update',
+                component: InformUpdate
             },
             {
                 path: 'QAndA',
