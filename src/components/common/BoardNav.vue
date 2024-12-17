c7794c1729d5d11aec0875f2e25eeafd5a89f439
seolbin01 <se01bin@suwon.ac.kr>
2024-12-17 12:15:42 +0900

    refactor: 공지사항 사이드바 경로 수정 #26

--- src/components/common/BoardNav.vue
+++ src/components/common/BoardNav.vue
@@ -6,7 +6,7 @@ const navItems = ref([]);
 
 navItems.value = [
   { id: 1, name: '공지사항', link: '/board/inform'},
-  { id: 2, name: 'Q&A', link: '/board/QAndA'},
+  { id: 2, name: 'Q&A', link: '/board/QNA'},
   { id: 3, name: 'FAQ', link: '/board/FAQ'},
 ];
 
