41c349c7139e65a9e6bfa1f37e4812f41e1d7880
seolbin01 <se01bin@suwon.ac.kr>
2024-12-18 21:14:52 +0900

    refactor: 파일 위치 수정 #66

--- src/router/promotion.js
+++ src/router/promotion.js
@@ -1,10 +1,15 @@
-import PromotionSave from "@/views/promotion/PromotionSave.vue";
-import PromotionAnalysis from "@/views/promotion/PromotionAnalysis.vue";
+import PromotionSave from "@/components/promotion/PromotionSave.vue";
+import PromotionAnalysis from "@/components/promotion/PromotionAnalysis.vue";
+import PromotionHome from "@/views/promotion/PromotionHome.vue";
 
 export default [
     {
         path: '/promotion',
         children: [
+            {
+                path: '',
+                component: PromotionHome
+            },
             {
                 path: 'save',
                 component: PromotionSave
