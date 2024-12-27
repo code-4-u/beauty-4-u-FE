edf4a908c24905f700b5159f537e29a7659e30f3
leebackcoding <lcy2825@gmail.com>
2024-12-27 14:38:05 +0900

    refactor: 연도 2010년까지 추가

--- src/components/goods/PromotionList.vue
+++ src/components/goods/PromotionList.vue
@@ -31,9 +31,21 @@ const sortOptions = [
 
 // 연도 옵션
 const yearOptions = [
-  {value: '2024', label: '2024년'},
-  {value: '2023', label: '2023년'},
-  {value: '2022', label: '2022년'}
+  { value: '2024', label: '2024년' },
+  { value: '2023', label: '2023년' },
+  { value: '2022', label: '2022년' },
+  { value: '2021', label: '2021년' },
+  { value: '2020', label: '2020년' },
+  { value: '2019', label: '2019년' },
+  { value: '2018', label: '2018년' },
+  { value: '2017', label: '2017년' },
+  { value: '2016', label: '2016년' },
+  { value: '2015', label: '2015년' },
+  { value: '2014', label: '2014년' },
+  { value: '2013', label: '2013년' },
+  { value: '2012', label: '2012년' },
+  { value: '2011', label: '2011년' },
+  { value: '2010', label: '2010년' },
 ];
 
 // 월 옵션
