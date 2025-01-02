7e91d6c9763a7d2da97f0eeb07463b3920ed95ab
seolbin01 <se01bin@suwon.ac.kr>
2025-01-03 03:38:32 +0900

    refactor: home 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -323,7 +323,7 @@ const saveEvent = async () => {
   try {
     const createScheduleReqData = {
       scheduleType: 'TEAMSCHEDULE',
-      scheduleUrl: `/teamspace/${authStore.deptCode}`,
+      scheduleUrl: ` `,
       scheduleReqDTO: {
         scheduleTitle: eventForm.title,
         scheduleContent: eventForm.content,
